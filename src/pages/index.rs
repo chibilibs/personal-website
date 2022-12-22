fc0b1aa3be58d52ab63fcc4cb825ea1402418738
Guilherme Menezes <46288058+thechibbis@users.noreply.github.com>
2022-12-22 12:28:00 -0300

    moved everything from dioxus to yew code. (#1)
    * yew: hello world
    
    * yew: moved everything from dioxus to yew code.
    
    * version: bump to 0.2.0-alpha
    
    * Delete dist directory

--- src/pages/index.rs
+++ src/pages/index.rs
@@ -1,9 +1,9 @@
-use axum::response::Html;
-use dioxus::prelude::*;
+use yew::prelude::*;
 
-pub async fn index_endpoint() -> Html<String> {
-    fn index(cx: Scope) -> Element {
-        const ASCII: &str = "⣿⣿⣿⣿⣯⣿⣿⠄⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠈⣿⣿⣿⣿⣿⣿⣆⠄
+#[function_component]
+pub fn Index() -> Html {
+    const ASCII: &str = "\
+⣿⣿⣿⣿⣯⣿⣿⠄⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠈⣿⣿⣿⣿⣿⣿⣆⠄
 ⢻⣿⣿⣿⣾⣿⢿⣢⣞⣿⣿⣿⣿⣷⣶⣿⣯⣟⣿⢿⡇⢃⢻⣿⣿⣿⣿⣿⢿⡄
 ⠄⢿⣿⣯⣏⣿⣿⣿⡟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣧⣾⢿⣮⣿⣿⣿⣿⣾⣷
 ⠄⣈⣽⢾⣿⣿⣿⣟⣄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣝⣯⢿⣿⣿⣿⣿
@@ -18,43 +18,29 @@ pub async fn index_endpoint() -> Html<String> {
 ⠄⠄⠄⠘⣿⡄⠄⠄⠙⢿⣿⣿⣾⣿⣷⣿⣿⣿⠟⠁⠄⠄⣿⣿⣾⣿⡟⣿⠄⠄
 ⠄⠄⠄⠄⢻⡇⠸⣆⠄⠄⠈⠻⣿⡿⠿⠛⠉⠄⠄⠄⠄⢸⣿⣇⣿⣿⢿⣿⠄⠄";
 
-        cx.render(rsx!{
-            head {
-                title { "rust web app" }
-                style { [include_str!("./styles/index.css")] }
-            }
-            body {
-                section { class: "container",
-                    div { class: "content header",
-                        div { class: "ascii",
-                            p { class: "ascii",
-                                "{ASCII}"
-                            }
-                        }
-                        h1 { "hey there!" }
-                    }
-                    div { class: "content about-me",
-                            div { class: "bg-about-me",
-                                h2 { "let about_me = r#''" }
-                                p { "Name: Guilherme de Oliveira Menezes" }
-                                p { "Nickname: thechibbis " }
-                                br { class: "brsmall" }
-                                p { "Description: I'm a brazilian Rust developer. I'm currently working on my personal projects and seeking knowledge especially on back-end" }
-                                p { "and of course increasing my abilities as a coder." }
-                                p { "Actually, I'd consider myself an intermediary programmer focused on backend with rust" }
-                                h2 { "''#" }
-                                h3 { "You can see some of my projects in the section below" }
-                        }
-                    }
-                    div { class: "content github-projects text-github" }
-                    div { class: "content github-projects" }
-                }
-            }
-    })
+    html! {
+        <section class="container">
+            <div class="content header">
+                <div class="ascii">
+                    <p class="ascii">{ASCII}</p>
+                </div>
+                <h1>{"Hey there!"}</h1>
+            </div>
+            <div class="content about-me">
+                <div class="bg-about-me">
+                    <h2>{"let about_me = r#''"}</h2>
+                    <p>{"Name: Guilherme de Oliveira Menezes"}</p>
+                    <p>{"Nickname: thechibbis"}</p>
+                    <br/>
+                    <p>{"Description: I'm a brazilian Rust developer. I'm currently working on my personal projects and seeking knowledge especially on back-end"}</p>
+                    <p>{"and of course increasing my abilities as a coder."}</p>
+                    <p>{"Actually, I'd consider myself an intermediary programmer focused on backend with rust"}</p>
+                    <h2>{"''#"}</h2>
+                    <h3>{"You can see some of my projects in the section below"}</h3>
+                </div>
+            </div>
+            <div class="content github-projects text-github"></div>
+            <div class="content github-projects"></div>
+        </section>
     }
-
-    let mut index = VirtualDom::new(index);
-    let _ = index.rebuild();
-
-    Html(dioxus::ssr::render_vdom(&index))
 }
