0fe05884016b9554a9e1c6bd9a33ba7462195663
Guilherme Menezes <guilhermeladins@gmail.com>
2022-12-22 10:39:18 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+pub mod pages;
+
+// I hate web dev
+use crate::pages::index::index_endpoint;
+
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `index`
+        .route("/", get(index_endpoint));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::info!("listening on http://{}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
---
+++ src/pages/index.rs
@@ -0,0 +1,60 @@
+use axum::response::Html;
+use dioxus::prelude::*;
+
+pub async fn index_endpoint() -> Html<String> {
+    fn index(cx: Scope) -> Element {
+        const ASCII: &str = "⣿⣿⣿⣿⣯⣿⣿⠄⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠈⣿⣿⣿⣿⣿⣿⣆⠄
+⢻⣿⣿⣿⣾⣿⢿⣢⣞⣿⣿⣿⣿⣷⣶⣿⣯⣟⣿⢿⡇⢃⢻⣿⣿⣿⣿⣿⢿⡄
+⠄⢿⣿⣯⣏⣿⣿⣿⡟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣧⣾⢿⣮⣿⣿⣿⣿⣾⣷
+⠄⣈⣽⢾⣿⣿⣿⣟⣄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣝⣯⢿⣿⣿⣿⣿
+⣿⠟⣫⢸⣿⢿⣿⣾⣿⢿⣿⣿⢻⣿⣿⣿⢿⣿⣿⣿⢸⣿⣼⣿⣿⣿⣿⣿⣿⣿
+⡟⢸⣟⢸⣿⠸⣷⣝⢻⠘⣿⣿⢸⢿⣿⣿⠄⣿⣿⣿⡆⢿⣿⣼⣿⣿⣿⣿⢹⣿
+⡇⣿⡿⣿⣿⢟⠛⠛⠿⡢⢻⣿⣾⣞⣿⡏⠖⢸⣿⢣⣷⡸⣇⣿⣿⣿⢼⡿⣿⣿
+⣡⢿⡷⣿⣿⣾⣿⣷⣶⣮⣄⣿⣏⣸⣻⣃⠭⠄⠛⠙⠛⠳⠋⣿⣿⣇⠙⣿⢸⣿
+⠫⣿⣧⣿⣿⣿⣿⣿⣿⣿⣿⣿⠻⣿⣾⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣹⢷⣿⡼⠋
+⠄⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣿⣿⣿⠄⠄
+⠄⠄⢻⢹⣿⠸⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣼⣿⣿⣿⣿⡟⠄⠄
+⠄⠄⠈⢸⣿⠄⠙⢿⣿⣿⣹⣿⣿⣿⣿⣟⡃⣽⣿⣿⡟⠁⣿⣿⢻⣿⣿⢿⠄⠄
+⠄⠄⠄⠘⣿⡄⠄⠄⠙⢿⣿⣿⣾⣿⣷⣿⣿⣿⠟⠁⠄⠄⣿⣿⣾⣿⡟⣿⠄⠄
+⠄⠄⠄⠄⢻⡇⠸⣆⠄⠄⠈⠻⣿⡿⠿⠛⠉⠄⠄⠄⠄⢸⣿⣇⣿⣿⢿⣿⠄⠄";
+
+        cx.render(rsx!{
+            head {
+                title { "rust web app" }
+                style { [include_str!("./styles/index.css")] }
+            }
+            body {
+                section { class: "container",
+                    div { class: "content header",
+                        div { class: "ascii",
+                            p { class: "ascii",
+                                "{ASCII}"
+                            }
+                        }
+                        h1 { "hey there!" }
+                    }
+                    div { class: "content about-me",
+                            div { class: "bg-about-me",
+                                h2 { "let about_me = r#''" }
+                                p { "Name: Guilherme de Oliveira Menezes" }
+                                p { "Nickname: thechibbis " }
+                                br { class: "brsmall" }
+                                p { "Description: I'm a brazilian Rust developer. I'm currently working on my personal projects and seeking knowledge especially on back-end" }
+                                p { "and of course increasing my abilities as a coder." }
+                                p { "Actually, I'd consider myself an intermediary programmer focused on backend with rust" }
+                                h2 { "''#" }
+                                h3 { "You can see some of my projects in the section below" }
+                        }
+                    }
+                    div { class: "content github-projects text-github" }
+                    div { class: "content github-projects" }
+                }
+            }
+    })
+    }
+
+    let mut index = VirtualDom::new(index);
+    let _ = index.rebuild();
+
+    Html(dioxus::ssr::render_vdom(&index))
+}
